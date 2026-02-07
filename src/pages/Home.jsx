5d290eb33b7276601dd37d2c2f16e7f938ef2557
raj-pandith <rajpandith1945@gmail.com>
2026-02-07 14:33:58 +0530

    added images2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -120,7 +120,6 @@ const Home = () => {
         </div>
       </section>
 
-
       {/* About Section */}
       <section className="py-16 lg:py-24 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
